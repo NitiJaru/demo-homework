a473828689f93807de8455c0ba900255e73919df
Earn <earndarkness@perfenterprise.com>
2018-06-22 14:33:40 +0700

    Implement clone command
    Signed-off-by: Earn <earndarkness@perfenterprise.com>

--- demo-homework/demo-homework/Program.cs
+++ demo-homework/demo-homework/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,32 @@ namespace demo_homework
     {
         static void Main(string[] args)
         {
+            const string gitUrl = @"https://github.com/taninpong/DemoTestforAcademy-true";
+            
+            const int githubIndexCount = 11;
+            var githubIndex = gitUrl.LastIndexOf("github.com");
+            var resultSubstring = gitUrl.Substring(githubIndex + githubIndexCount);
+            var resultSplited = resultSubstring.Split('/');
+            var directoryName = resultSplited.FirstOrDefault().ToLower();
+
+            var navigateToRootCommand = "cd /";
+            var navigateToGitCommand = "cd git";
+            var createDirectoryCommand = $"md {directoryName}";
+            var navigateToDirectoryWasCreated = $"cd {directoryName}";
+            var cloneCommand = $"git clone {gitUrl}";
+
+            var command =
+                $"{navigateToRootCommand}" +
+                $"&{navigateToGitCommand}" +
+                $"&{createDirectoryCommand}" +
+                $"&{navigateToDirectoryWasCreated}" +
+                $"&{cloneCommand}";
+
+            Console.WriteLine($"Process is starting to clone project {directoryName}\n");
+            Process.Start(new ProcessStartInfo("cmd", $"/c {command}") { UseShellExecute = false }).WaitForExit();
+            Console.WriteLine("\nProcess is end. Clone was completed");
+
         }
+
     }
 }
