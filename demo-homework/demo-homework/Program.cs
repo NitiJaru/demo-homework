fa240c5f94e89b3dd42a9a5cdccbb8fd7ab93c06
Earn <earndarkness@perfenterprise.com>
2018-06-22 16:41:50 +0700

    Add description
    Signed-off-by: Earn <earndarkness@perfenterprise.com>

--- demo-homework/demo-homework/Program.cs
+++ demo-homework/demo-homework/Program.cs
@@ -20,8 +20,12 @@ namespace demo_homework
                 $"\nSkip: {result.Skip}");
         }
 
+        /// <summary>
+        /// Git clone (Example)
+        /// </summary>
         private static void GitClone()
         {
+            // Prepare url
             const string gitUrl = @"https://github.com/taninpong/DemoTestforAcademy-true";
 
             // Find Directory name from URL
@@ -31,13 +35,12 @@ namespace demo_homework
             var resultSplited = resultSubstring.Split('/');
             var directoryName = resultSplited.FirstOrDefault().ToLower();
 
-            // TODO: Crate Directory to local (must change later before Using Azure Function App)
+            // Prepare commands
             var navigateToRootCommand = "cd /";
             var navigateToGitCommand = "cd git";
             var createDirectoryCommand = $"md {directoryName}";
             var navigateToDirectoryWasCreated = $"cd {directoryName}";
             var cloneCommand = $"git clone {gitUrl}";
-
             var command =
                 $"{navigateToRootCommand}" +
                 $"&{navigateToGitCommand}" +
@@ -45,30 +48,42 @@ namespace demo_homework
                 $"&{navigateToDirectoryWasCreated}" +
                 $"&{cloneCommand}";
 
+            // Process git clone commands
             Console.WriteLine($"Process is starting to clone project {directoryName}\n");
             Process.Start(new ProcessStartInfo("cmd", $"/c {command}") { UseShellExecute = false }).WaitForExit();
             Console.WriteLine("\nProcess is end. Clone was completed");
         }
 
+        /// <summary>
+        /// Read data from test (Example)
+        /// </summary>
+        /// <returns></returns>
         private static HomeworkTestResult TestResult()
         {
+            // Prepare commands
+            var navigateToRootCommand = "cd /";
+            var navigateToGitCommand = "cd Git/taninpong/DemoTestforAcademy-true/Training.TestDrivenDevelopement/TDD.TestProject";
             var command =
-                $"cd /" +
-                $"&cd Git/taninpong/DemoTestforAcademy-true/Training.TestDrivenDevelopement/TDD.TestProject" +
+                $"{navigateToRootCommand}" +
+                $"&{navigateToGitCommand}" +
                 $"&dotnet test";
 
+            // process test command
             var process = new Process
             {
                 StartInfo = new ProcessStartInfo("cmd", $"/c {command}") { UseShellExecute = false, RedirectStandardOutput = true }
             };
             process.Start();
+
+            // Get data from test
             var data = process.StandardOutput.ReadToEnd();
 
             // HACK: Fix username
             const string username = "taninpong";
+
+            // Convert to HomeworkTestResult model
             var convertor = new HomeworkResultConverter();
             var result = convertor.GetHomeworkResult(username, data);
-
             return result;
         }
 
